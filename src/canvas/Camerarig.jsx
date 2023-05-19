0b89b941f4011015cad4267518da99e0d3f76ee4
Magmastorm3007 <shashwat.nath@hotmail.com>
2023-05-19 20:27:01 +0530

    Colour picker implemented successfully

--- src/canvas/Camerarig.jsx
+++ src/canvas/Camerarig.jsx
@@ -7,11 +7,30 @@ function Camerarig({children}) {
     const group =useRef()
     const snap=useSnapshot(state);
     useFrame((state,delta)=>{
-        
-  
+        const isbreakpoint=window.innerWidth<=1260;
+        const ismobile=window.innerWidth<=600;
+
+    //set the initial position of the model
+    let targetPosition=[-0.4,0,2];
+    if(snap.intro){
+      if(isbreakpoint) targetPosition=[0,0,2];
+      if(ismobile) targetPosition=[0,0.2,2.5];
+      else{
+        if(ismobile) 
+        targetPosition=[0,0,2.5];
+        else
+        targetPosition=[0,0,2];
+
+      }
+    }
+
+    //set the position
+    easing.damp3(state.camera.position,targetPosition,0.25,delta);
+
+
     easing.dampE(
         group.current.rotation,
-        [state.pointer.y/10,-state.pointer.x/5,0],0.25,delta
+        [state.pointer.y/10,-state.pointer.x/5,0,0.25,delta]
     )
     })
 
