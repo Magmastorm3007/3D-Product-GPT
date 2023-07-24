0b1c58713e5d7cf0272087d44793639414b0ca92
Magmastorm3007 <shashwat.nath@hotmail.com>
2023-07-24 15:46:27 +0530

    change source path

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -8,7 +8,7 @@ import state from '../store';
 
 const Shirt = () => {
   const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('../src/assets/shirt_baked.glb');
+  const { nodes, materials } = useGLTF('/assets/shirt_baked.glb');
 
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
