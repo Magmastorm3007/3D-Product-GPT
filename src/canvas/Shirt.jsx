ef28539932c29f49b392ae0fbffcf2787c3dc8a0
Magmastorm3007 <shashwat.nath@hotmail.com>
2023-07-24 16:05:28 +0530

    change

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -8,7 +8,7 @@ import state from '../store';
 
 const Shirt = () => {
   const snap = useSnapshot(state);
-  const { nodes, materials } = useGLTF('/assets/shirt_baked.glb');
+  const { nodes, materials } = useGLTF('3D-Product-GPT/assets/shirt_baked.glb');
 
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
